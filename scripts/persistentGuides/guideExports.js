35d33070a144d350de8c5400b4d69d35fe3f48ea
haruny8 <69612672+haruny8@users.noreply.github.com>
2026-07-18 12:03:08 -0300

    Clean up guideExports.js by removing unused imports
    Removed unused guide and tool imports, retaining only essential functions for the GuidedGenerations extension.

--- scripts/persistentGuides/guideExports.js
+++ scripts/persistentGuides/guideExports.js
@@ -1,6 +1,9 @@
 /**
  * @file Central import/export hub for all GuidedGenerations extension modules.
  * This file serves as a single point of entry for all imports, eliminating path depth issues.
+ *
+ * TRIMMED VERSION: Only Impersonate (1st/2nd/3rd) + Guided Continue/Response/Swipe are retained.
+ * All Persistent Guides, Tools, Stat Tracker, and Fun Prompts features have been removed.
  */
 
 // External dependencies (SillyTavern)
@@ -53,26 +56,14 @@ function isGroupChat() {
 // Settings management functions - imported from index.js
 import { loadSettings, updateSettingsUI, addSettingsEventListeners, debugProfileSystem, getDebugMessages, clearDebugMessages, getDebugMessagesAsText, debugError } from '../../index.js';
 
-// Default settings object
+// Default settings object (small local subset used by settingsPanel.js's "Default" buttons)
 const defaultSettings = {
-    autoTriggerClothes: false,
-    autoTriggerState: false,
-    autoTriggerThinking: false,
-    enableAutoCustomAutoGuide: false,
     showImpersonate1stPerson: true,
     showImpersonate2ndPerson: false,
     showImpersonate3rdPerson: false,
     showGuidedContinue: false,
     showGuidedResponse: true,
     showGuidedSwipe: true,
-    showSimpleSendButton: false,
-    showRecoverInputButton: false,
-    showEditIntrosButton: false,
-    showCorrectionsButton: false,
-    showSpellcheckerButton: false,
-    showClearInputButton: false,
-    showUndoButton: false,
-    showRevertButton: false,
     integrateQrBar: true,
     debugMode: false,
     injectionEndRole: 'system'
@@ -81,29 +72,6 @@ const defaultSettings = {
 // Utility functions
 import { handleSwitching, getProfileApiType, getPresetsForApiType, getCurrentProfile, getProfileList, switchToProfile, switchToPreset, withProfile, getConnectApiMap, initializeEventListeners, extractApiIdFromApiType } from '../utils/presetUtils.js';
 
-// Guide functions
-import situationalGuide from './situationalGuide.js';
-import thinkingGuide from './thinkingGuide.js';
-import clothesGuide from './clothesGuide.js';
-import stateGuide from './stateGuide.js';
-import rulesGuide from './rulesGuide.js';
-import customGuide from './customGuide.js';
-import customAutoGuide from './customAutoGuide.js';
-import editGuides from './editGuides.js';
-import showGuides from './showGuides.js';
-import flushGuides from './flushGuides.js';
-import funGuide from './funGuide.js';
-import trackerGuide from './trackerGuide.js';
-import { executeTracker, checkAndExecuteTracker, createTrackerNote } from './trackerLogic.js';
-import { runGuideScript } from './runGuide.js';
-import { updateCharacter } from './updateCharacter.js';
-
-// Tool functions
-import { corrections } from '../tools/corrections.js';
-import { spellchecker } from '../tools/spellchecker.js';
-import editIntros from '../tools/editIntros.js';
-import clearInput from '../tools/clearInput.js';
-
 // Main script functions
 import { guidedSwipe, generateNewSwipe } from '../guidedSwipe.js';
 import { guidedContinue, initGuidedContinueListeners, undoLastGuidedAddition, revertToOriginalGuidedContinue } from '../guidedContinue.js';
@@ -111,8 +79,6 @@ import { guidedResponse } from '../guidedResponse.js';
 import { guidedImpersonate } from '../guidedImpersonate.js';
 import { guidedImpersonate2nd } from '../guidedImpersonate2nd.js';
 import { guidedImpersonate3rd } from '../guidedImpersonate3rd.js';
-import { simpleSend } from '../simpleSend.js';
-import { recoverInput } from '../inputRecovery.js';
 import { loadSettingsPanel } from '../settingsPanel.js';
 
 // Export everything
@@ -124,7 +90,7 @@ export {
     debugLog,
     debugWarn,
     debugError,
-    
+
     // SillyTavern dependencies
     chat,
     eventSource,
@@ -132,7 +98,7 @@ export {
     saveChatConditional,
     addOneMessage,
     renderExtensionTemplateAsync,
-    
+
     // Utility functions
     handleSwitching,
     getProfileApiType,
@@ -145,32 +111,7 @@ export {
     getConnectApiMap,
     initializeEventListeners,
     extractApiIdFromApiType,
-    
-    // Guides
-    runGuideScript,
-    clothesGuide,
-    stateGuide,
-    thinkingGuide,
-    situationalGuide,
-    rulesGuide,
-    customGuide,
-    customAutoGuide,
-    trackerGuide,
-    executeTracker,
-    checkAndExecuteTracker,
-    createTrackerNote,
-    funGuide,
-    flushGuides,
-    showGuides,
-    editGuides,
-    updateCharacter,
-    
-    // Tools
-    clearInput,
-    corrections,
-    editIntros,
-    spellchecker,
-    
+
     // Main script functions
     guidedSwipe,
     generateNewSwipe,
@@ -182,10 +123,8 @@ export {
     guidedImpersonate,
     guidedImpersonate2nd,
     guidedImpersonate3rd,
-    simpleSend,
-    recoverInput,
     loadSettingsPanel,
-    
+
     // Settings and other
     loadSettings,
     updateSettingsUI,
@@ -197,7 +136,7 @@ export {
     getPreviousImpersonateInput,
     setLastImpersonateResult,
     getLastImpersonateResult,
-    
+
     // Debug logging functions
     getDebugMessages,
     clearDebugMessages,
